c2d79c73d13f5d2740e5f83f951e2edf7daf1a4a
Eko Kurniadi <eko.kurniadi@majoo.id>
2022-10-01 13:23:43 +0700

    up

--- lib/core/helpers/constant_helper.dart
+++ lib/core/helpers/constant_helper.dart
@@ -2,16 +2,18 @@ abstract class ConstantHelper {
   static const String applicationJson = 'application/json';
 
   // Database Setting Dev
-  static const String host = 'localhost';
-  static const String userName = 'postgres';
-  static const String password = 'password';
-  static const String databaseName = 'kampus';
+  static const String host = 'ec2-35-170-146-54.compute-1.amazonaws.com';
+  static const String userName = 'efiqackaledkfs';
+  static const String password =
+      'c4d5254539beeea1b7d2960972b961352c45ef465981a804abffafae2e55b5b3';
+  static const String databaseName = 'd41067o1fbvvsm';
   static const int port = 5432;
 
   // Database Setting Prod
-  static const String hostProd = 'localhost';
-  static const String userNameProd = 'postgres';
-  static const String passwordProd = 'password';
-  static const String databaseNameProd = 'kampus';
+  static const String hostProd = 'ec2-35-170-146-54.compute-1.amazonaws.com';
+  static const String userNameProd = 'efiqackaledkfs';
+  static const String passwordProd =
+      'c4d5254539beeea1b7d2960972b961352c45ef465981a804abffafae2e55b5b3';
+  static const String databaseNameProd = 'd41067o1fbvvsm';
   static const int portProd = 5432;
 }
